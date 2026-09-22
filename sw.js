16dca670233fd3acc1b9d7553867e3fe5f1dc135
guilhermepimentelgp <guilhermermp40@gmail.com>
2026-09-21 22:46:26 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'controle-financeiro-v2';
-const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'controle-financeiro-v3';
+const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-192-maskable.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
